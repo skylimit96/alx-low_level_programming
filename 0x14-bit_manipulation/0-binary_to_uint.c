4adfae820397309b4b6bd5eeca65c8390844b623
skylimit96 <a.sehlaoui96@gmail.com>
2023-05-27 01:57:19 -0700

    first task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_unsint - Transform binary number to unsigned int
+ * @x: char string
+ * Return: Transform decimal number or 0 if it s unconvertable char
+ */
+unsigned int binary_to_unsint(const char *x)
+{
+	unsigned int tot, pwr;
+	int lenght;
+
+	if (x == NULL)
+		return (0);
+
+	for (lenght = 0; x[lenght]; lenght++)
+	{
+		if (x[lenght] != '0' && x[lenght] != '1')
+			return (0);
+	}
+
+	for (pwr = 1, tot = 0, lenght--; lenght >= 0; lenght--, pwr *= 2)
+	{
+		if (x[lenght] == '1')
+			tot += pwr;
+	}
+
+	return (tot);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+unsigned int binary_to_unsint(const char *x);
+
+#endif
